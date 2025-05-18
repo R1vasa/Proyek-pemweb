9bdcec83565316c113b6331301384782daf27a32
unknown <altmifzal@gmail.com>
2025-05-18 12:36:10 +0700

    component-tahap-awal

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function index(){
         return view('pages/index');
+    }
+
+    public function get_user()
+    {
+        $users = User::all();
+        if (request()->wantsJson()) {
+            return response()->json($users);
+        }
+        return $users;
+    }
+
+    public function get_user_by_id($id)
+    {
+        $user = User::find($id);
+        if (request()->wantsJson()) {
+            return response()->json($user);
+        }
+        return $user;
     }
 }
